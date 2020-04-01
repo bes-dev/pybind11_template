d03268ffe476618b0f351bc661e2a56ac220bc70
BelBES <sergei.o.belousov@gmail.com>
2020-04-01 16:06:48 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <iostream>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv_wrapper/ndarray_converter.h"
 
 namespace py = pybind11;
 
@@ -8,8 +10,10 @@ class Example {
 public:
     Example() {}
     virtual std::string hello() {
-        cv::Mat img(1000, 1000, CV_8U);
         return std::string("hello world");
+    }
+    virtual void imshow(std::string title, cv::Mat& img) {
+        cv::imshow(title, img);
     }
 };
 
@@ -24,10 +28,19 @@ public:
             hello,
         );
     }
+    void imshow(std::string title, cv::Mat& img) override {
+        PYBIND11_OVERLOAD_PURE(
+            void, /* Return type */
+            Example,      /* Parent class */
+            imshow,          /* Name of function in C++ (must match Python name) */
+            title, img      /* Argument(s) */
+        );
+    }
 };
 
 PYBIND11_MODULE(class_wrapper, m) {
     py::class_<Example, PyExample>(m, "Example")
         .def(py::init<>())
-        .def("hello", &Example::hello);
+        .def("hello", &Example::hello)
+        .def("imshow", &Example::imshow);
 }
